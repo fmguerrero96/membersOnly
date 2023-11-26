130808c53e9e77a3b4d1df4fcbb91e821aa44f85
Misael Guerrero <mguerrerowest@gmail.com>
2023-11-25 20:23:53 -0500

    Define user schema

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    first_name: { type: String, required: true, maxLength: 100 },
+    username: { type: String, required: true, maxLength: 100, unique: true },
+    password: { type: String, required: true, maxLength: 100 },
+    is_admin: {type: Boolean, default: false, required: true},
+    is_vip: {type: Boolean, default: false, required: true},
+})
+
+module.exports = mongoose.model("User", UserSchema)
