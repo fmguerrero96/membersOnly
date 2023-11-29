a32344ad0b748c3949ba612e5da9d286e8d8fb27
Misael Guerrero <mguerrerowest@gmail.com>
2023-11-29 13:50:31 -0500

    Get rid of 'routes/members.js' and move those routes to 'routes/index.js'

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+// Require controller modules.
+const user_controller = require('../controllers/userController')
+
+// User Routes //
+
+//GET home page
 router.get('/', function(req, res, next) {
-  res.redirect('/members')
-});
+    res.render('index', { title: 'Express', user: req.user });
+  });
+
+//Get request for displaying signup form 
+router.get('/signup', user_controller.sign_up);
+
+//POST request for creating new users
+router.post('/signup', user_controller.create_user);
 
 module.exports = router;
