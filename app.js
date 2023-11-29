88b6ca980f62ad8dfc6abe20689828406e539085
Misael Guerrero <mguerrerowest@gmail.com>
2023-11-29 14:44:25 -0500

    Set up the local startegy, serialization and add a POST route for logging in

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport')
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+const bcrypt = require('bcryptjs');
+const User = require('./models/user')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const membersRouter = require('./routes/members')
 
 const app = express();
 
@@ -24,6 +28,43 @@ async function main() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//setting up the LocalStrategy
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      };
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    };
+  })
+);
+
+//Serialization 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  };
+});
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -32,7 +73,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/members', membersRouter)
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
