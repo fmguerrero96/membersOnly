ef64c918f77a70e108f53d4cbb0fe2c22cf0b7f7
Misael Guerrero <mguerrerowest@gmail.com>
2023-11-29 15:24:03 -0500

    Remove unnecessary import

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport')
 
 // Require controller modules.
 const user_controller = require('../controllers/userController')
